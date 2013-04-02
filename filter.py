612d0117676bab951421daee24d5c75da493f37f
Paulo Roberto Urio <jimmyskull@gmail.com>
2013-04-02 07:32:53 -0300

    Computing the star radius based on tabled temperature
    Signed-off-by: Paulo Roberto Urio <jimmyskull@gmail.com>

--- filter.py
+++ filter.py
@@ -9,18 +9,62 @@
 # http://heasarc.gsfc.nasa.gov/W3Browse/all/hipparcos.html
 # http://en.wikipedia.org/wiki/Mass%E2%80%93luminosity_relation
 import sys
-from math import pi, cos, sin, tan, sqrt
+from math import pi, cos, sin, tan, sqrt, log10
 
 PARSEC = 3.0856775814671900 * 10 ** 16
 
 class Sun:
-	MASS = 1.98855e30
-	LUMINOSITY = 1 # 1.9e-16
-	DISTANCE = 4.848e-6
-	VMAG = -26.74
-	(X, Y, Z) = (0, 0, 0)
+	MASS = 1.98855e30 # kg
+	LUMINOSITY = 1.9e-16 # W
+	DISTANCE = 4.848e-6 # parsec
+	VMAG = -26.74 # mag
+	RADIUS = 6.955 * 10 ** 8 # meters
+	TEMPERATURE = 5778 # Kelvin
+	(X, Y, Z) = (0, 0, 0) # parsec
 
 cot = lambda x: 1 / tan(x)
+
+class Radius(object):
+	# http://www.uni.edu/morgans/astro/course/Notes/section2/spectraltemps.html
+	# Absolute Magnitude (mag): Temperature (K)
+	TABLE = { -4.5: 54000, -4.0: 45000, -3.9: 43300, -3.8: 40600,
+			-3.6: 37800, -3.3: 29200, -2.3: 23000, -1.9: 21000,
+			-1.1: 17600, -.4: 15200, .0: 14300, .3: 13500, .7: 12300,
+			+1.1: 11400, 1.5: 9600, 1.7: 9330, 1.8: 9040, 2.: 8750,
+			2.1: 8480, 2.2: 8310, 2.4: 7920, 3.: 7350, 3.3: 7050,
+			3.5: 6850, 3.7: 6700, 4.: 6550, 4.3: 6400, 4.4: 6300,
+			4.7: 6050, 4.9: 5930, 5.: 5800, 5.2: 5660, 5.6: 5440,
+			6.: 5240, 6.2: 5110, 6.4: 4960, 6.7: 4800, 7.1: 4600,
+			7.4: 4400, 8.1: 4000, 8.7: 3750, 9.4: 3700, 10.1: 3600,
+			10.7: 3500, 11.2: 3400, 12.3: 3200, 13.4: 3100, 13.9: 2900,
+			14.4: 2700}
+	
+	def __init__(self, star):
+		self.star = star
+		self.get_temperature()
+			
+	def _apparent_magnitude(self, absolute_magnitude, distance):
+		return absolute_magnitude - 5 * (1 - log10(distance))
+	
+	def get_temperature(self):
+		best_match = None
+		best_approx = None
+		for mag in self.TABLE:
+			vmag = self._apparent_magnitude(mag, self.star.coord.distance())
+			approx = abs(vmag - self.star.vmag)
+			if not best_match or approx < best_approx:
+				best_match = mag
+				best_approx = approx
+		return self.TABLE[best_match]
+
+	def __truediv__(self, b):
+		return float(self) / b
+
+	def __float__(self):
+		# http://en.wikipedia.org/wiki/Stefan%E2%80%93Boltzmann_law
+		temperature = (Sun.TEMPERATURE / self.get_temperature()) ** 2
+		luminosity = sqrt(self.star.vmag.luminosity() / Sun.LUMINOSITY)
+		return Sun.RADIUS * temperature * luminosity
 
 class Declination(object):
 	def __init__(self, d, m, s):
@@ -85,7 +129,7 @@ class ProperMotion(object):
 	def __str__(self):
 		return '{RA} {DE}'.format(RA=self.pmRA, DE=self.pmDE)
 
-class Mass(object):
+class VMagnitude(object):
 	def __init__(self, Vmag, star):
 		self.Vmag = float(Vmag)
 		self.star = star
@@ -93,20 +137,26 @@ class Mass(object):
 	def luminosity(self):
 		# http://en.wikipedia.org/wiki/Luminosity#Apparent
 		mag = 10 ** ((self.Vmag - Sun.VMAG) / -2.5)
-		d = (Sun.DISTANCE / self.star.position.distance()) ** 2
+		d = (Sun.DISTANCE / self.star.coord.distance()) ** 2
 		return (Sun.LUMINOSITY * mag) / d
 		
 	def radius(self):
 		# http://physics.ucsd.edu/students/courses/winter2008/managed/physics223/documents/Lecture7%13Part3.pdf
-		return float(self) ** (15 / 19)
+		return float(self) ** (.9)
 	
-	def __float__(self):
+	def mass(self):
 		a = 3.5
 		mag = self.luminosity() / Sun.LUMINOSITY
 		return Sun.MASS * mag ** (1 / a)
 	
+	def __rsub__(self, a):
+		return a - float(self)
+
+	def __float__(self):
+		return self.Vmag
+	
 	def __str__(self):
-		return str(float(self))
+		raise str(self.Vmag)
 
 class Star(object):
 	def __init__(self, Catalog, HIP, Proxy, RAhms, DEdms, Vmag, VarFlag, \
@@ -120,9 +170,10 @@ class Star(object):
 		m_HIP, theta, rho, e_rho, dHp, e_dHpm, Survey, Chart, Notes, \
 		HD, BD, CoD, CPD, VIred, SpType, r_SpType):
 		self.id = int(HIP)
-		self.position = CelestialCoordinate(RAhms, DEdms, Plx)
+		self.coord = CelestialCoordinate(RAhms, DEdms, Plx)
 		self.proper_motion = ProperMotion(pmRA, pmDE)
-		self.mass = Mass(Vmag, self)
+		self.vmag = VMagnitude(Vmag, self)
+		self.radius = Radius(self)
 		self.info = (('\nID = %(HIP)s\nRaw input\n'
 				    '\tRAhms = %(RAhms)s\n'
 					'\tDEdms = %(DEdms)s\n'
@@ -137,30 +188,32 @@ class Star(object):
 					'\tY = {Y} parsecs\n'
 					'\tZ = {Z} parsecs\n'
 					'\tDistance = {distance} parsecs\n'
-					'\tLuminosity = {luminosity} W\n'
-					'\tMass = {mass} kg\n'
-					'\tRadius = {radius} meters') % locals()).format(
-						RA=self.position.RA,
-						DE=self.position.DE,
-						X=self.position.x(),
-						Y=self.position.y(),
-						Z=self.position.z(),
-						distance=self.position.distance(),
-						luminosity=self.mass.luminosity(),
-						mass=self.mass,
-						radius=self.mass.radius())
+					'\tLuminosity = {luminosity} L☉\n'
+					'\tMass = {mass} M☉\n'
+					'\tTemperature = {temperature} K☉\n'
+					'\tRadius = {radius} R☉') % locals()).format(
+						RA=self.coord.RA,
+						DE=self.coord.DE,
+						X=self.coord.x(),
+						Y=self.coord.y(),
+						Z=self.coord.z(),
+						distance=self.coord.distance(),
+						luminosity=self.vmag.luminosity() / Sun.LUMINOSITY,
+						mass=float(self.vmag.mass()) / Sun.MASS,
+						temperature=self.radius.get_temperature() / Sun.TEMPERATURE,
+						radius=self.radius / Sun.RADIUS)
 
 
 	def __str__(self):
 		return '{id} {position} {motion} {mass}'.format(id=self.id, \
-			position=self.position, motion=self.proper_motion, \
-			mass=self.mass)
+			position=self.coord, motion=self.proper_motion, \
+			mass=self.vmag.mass())
 
 def process_line(line):
 	try:
 		star = Star(*line.split('|'))
 		# Filter criteria evaluation
-		if (star.id < 5):
+		if star.id < 5:
 			print(star.info)
 			print('Output line')
 			print(star)
@@ -176,7 +229,8 @@ def process_line(line):
 	except KeyboardInterrupt:
 		exit(0)
 
-input_data = sys.stdin.readlines()
-for line in input_data:
-	process_line(line)
+if __name__ == '__main__':
+	input_data = sys.stdin.readlines()
+	for line in input_data:
+		process_line(line)
 
